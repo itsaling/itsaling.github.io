994ad770768bf287a34eda230292bc5c56210fc3
latsadavanh le <lle7@students.towson.edu>
2019-10-23 07:47:27 -0400

    profile change

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function attachNav() {
   $("#nav-placeholder").load("nav.html");
 }
+
+function navActive(e) {
+  let activeLink = document.querySelectorAll(".active");
+  [].forEach.all(activeLink, function(link) {
+    link.classList.remove("active");
+  });
+  e.target.className = "active";
+}
